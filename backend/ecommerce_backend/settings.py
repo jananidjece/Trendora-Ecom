105399b6a2134923aecf969c804bc791764930f4
jananidjece@ <jananidjece@gmail.com>
2026-01-28 13:42:27 +0530

    Fconnect

--- backend/ecommerce_backend/settings.py
+++ backend/ecommerce_backend/settings.py
@@ -153,6 +153,7 @@ CSRF_TRUSTED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
     'https://*.render.com',
+    'https://frontend-nine-beige-51.vercel.app',
 ]
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
