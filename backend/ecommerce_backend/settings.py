a7acee719df8daa97ef8d3bccc876c83727a7550
jananidjece@ <jananidjece@gmail.com>
2026-01-29 22:46:08 +0530

    Fix build compatibility

--- backend/ecommerce_backend/settings.py
+++ backend/ecommerce_backend/settings.py
@@ -171,6 +171,7 @@ if os.environ.get('CLOUDINARY_CLOUD_NAME'):
         'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
     }
     DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
     STORAGES = {
         "default": {
             "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
@@ -180,6 +181,7 @@ if os.environ.get('CLOUDINARY_CLOUD_NAME'):
         },
     }
 else:
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
     STORAGES = {
         "default": {
             "BACKEND": "django.core.files.storage.FileSystemStorage",
